2f0dad31ffabf7a0c0c1d6a3232a7451e046b8f1
shamim7999 <10shamimsarker01@gmail.com>
2024-02-25 16:38:11 +0600

    Added Basic Security

---
+++ src/main/java/org/dsi/blogapp/config/SecurityConfig.java
@@ -0,0 +1,19 @@
+package org.dsi.blogapp.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+public class SecurityConfig {
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf((csrf) -> csrf.disable())
+                .authorizeHttpRequests((authorize) -> {
+                            authorize.anyRequest()
+                                    .authenticated();
+                        }
+                );
+        return http.build();
+    }
+}
